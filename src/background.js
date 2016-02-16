4b3c8cd8d7730296058e94474f8e539a4f3130b3
nijk <nick@codenamenick.co.uk>
2016-02-16 23:57:38 +0000

    Add pervious timings

--- src/background.js
+++ src/background.js
@@ -4,8 +4,17 @@ chrome[roe].onMessage.addListener(
     function(request, sender, sendResponse) {
         // This cache stores page load time for each tab, so they don't interfere
         chrome.storage.local.get('cache', function(data) {
-            if (!data.cache) data.cache = {};
-            data.cache['tab' + sender.tab.id] = request.timing;
+            var tabName = 'tab' + sender.tab.id;
+
+            if (!data.cache) {
+                data.cache = {};
+            }
+
+            if (!data.cache[tabName]) {
+                data.cache[tabName] = [];
+            }
+
+            data.cache[tabName].push(request.timing);
             chrome.storage.local.set(data);
         });
         chrome.browserAction.setBadgeText({text: request.time, tabId: sender.tab.id});
--- src/popup.js
+++ src/popup.js
@@ -17,8 +17,10 @@ function set(id, start, length, noacc) {
 
 chrome.tabs.getSelected(null, function (tab) {
     chrome.storage.local.get('cache', function(data) {
-        var t = data.cache['tab' + tab.id],
-        start = t.redirectStart == 0 ?t.fetchStart : t.redirectStart;
+        var allTimingData = data.cache['tab' + tab.id],
+            lastItem = allTimingData.length - 1,
+            t = allTimingData[lastItem],
+            start = (t.redirectStart == 0) ? t.fetchStart : t.redirectStart;
 
         total = t.loadEventEnd - start;
 
@@ -35,5 +37,16 @@ chrome.tabs.getSelected(null, function (tab) {
         set('load', t.loadEventStart - start, t.loadEventEnd - t.loadEventStart);
 
         document.getElementById('totalTotal').innerHTML = (total / 1000).toPrecision(3).substring(0, 4).toString() + ' seconds';
+
+        var prevTimings = allTimingData.map(function(val, index){
+            if (index < lastItem) {
+                var start = (val.redirectStart == 0) ? val.fetchStart : val.redirectStart,
+                    total = val.loadEventEnd - start;
+
+                return (total / 1000).toPrecision(3).substring(0, 4).toString() + ' seconds';
+            }
+        });
+
+        document.getElementById('prevTotal').innerHTML = prevTimings;
     });
 });
